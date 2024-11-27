2184c7c29523ad2eac928f4be39df82b5b582cdf
Aamil13 <aamil.shafi13@gmail.com>
2024-11-28 00:03:16 +0530

    updated packages

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -5,5 +5,17 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    // Prevent mismatch during hydration
+    return <div className="opacity-0">{children}</div>;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Tm3gZxih4lFN7AK25ckCKYBeMSwKbaN0G6ZSRVEP6m0=\"\n}"
