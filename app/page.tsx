9a561fcd9803c1db0931d47be8970a9780f2a1b6
Aamil13 <aamil.shafi13@gmail.com>
2024-11-28 00:13:23 +0530

    added check for document

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-
+"use client"
 import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -7,9 +7,19 @@ import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Projects from "@/components/Projects";
 import {projectsData} from "@/data/index"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    // Prevent mismatch during hydration
+    return <div className="opacity-0">wait...</div>;
+  }
 
   return (
       <main className="relative bg-black-100 flex justify-center
